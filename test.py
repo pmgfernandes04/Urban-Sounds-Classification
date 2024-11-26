bae70d8123af07fc28e3f8091556a447cf99dab8
rafa907p <rafa907ak@gmail.com>
2024-11-26 23:42:05 +0000

    Can start model testing
    I believe all functions are now up and capable of running.
    
    Apologies for making test_model so complex...

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-pair = (4,2,3)
+import random
 
-print(pair[1:])
+i = 1
+
+# Enumerates every fold number in the current train_dataframe
+train_folds = [fold for fold in range(1, 11) if fold != i]
+
+print(train_folds)
+
+# Which allows us to randomly select one of those folds -> validation fold
+validation_fold = random.choice(train_folds)
+
+print(validation_fold)
